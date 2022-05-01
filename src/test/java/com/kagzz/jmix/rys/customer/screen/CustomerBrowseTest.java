06ef94c31662ef03d492ac14f921764db1394c38
Alex Mwangi <62346007+alexkagzz@users.noreply.github.com>
2022-05-01 19:02:41 +0300

    Create test to ensure that the customer edit form is shown (#14)
    Co-authored-by: amwangi10 <amwangi10@safaricom.co.ke>

--- src/test/java/com/kagzz/jmix/rys/customer/screen/CustomerBrowseTest.java
+++ src/test/java/com/kagzz/jmix/rys/customer/screen/CustomerBrowseTest.java
@@ -4,10 +4,10 @@ import com.kagzz.jmix.rys.RentYourStuffApplication;
 import com.kagzz.jmix.rys.customer.entity.Customer;
 import com.kagzz.jmix.rys.entity.Address;
 import io.jmix.core.DataManager;
-import io.jmix.core.security.SystemAuthenticator;
 import io.jmix.ui.Screens;
-import io.jmix.ui.component.Component;
+import io.jmix.ui.component.Button;
 import io.jmix.ui.component.Table;
+import io.jmix.ui.screen.Screen;
 import io.jmix.ui.testassist.UiTestAssistConfiguration;
 import io.jmix.ui.testassist.junit.UiTest;
 import org.jetbrains.annotations.NotNull;
@@ -22,7 +22,6 @@ import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -70,6 +69,46 @@ class CustomerBrowseTest {
 //       Expect:
         assertThat(getFirstLoadedCustomer(customerBrowse)).isEqualTo(customer);
     }
+
+    @Test
+    void given_oneCustomerExists_when_editingCustomer_then_editCustomerEditorIsShown(Screens screens) {
+
+//        Given:
+        CustomerBrowse customerBrowse = openCustomerBrowse(screens);
+
+//        And
+        Customer firstCustomer = getFirstLoadedCustomer(customerBrowse);
+
+//        And
+        selectCustomerInTable(customerBrowse, firstCustomer);
+
+//        When
+        getButton(customerBrowse, "editBtn").click();
+
+//       Then:
+        CustomerEdit customerEdit = getScreenOfType(screens, CustomerEdit.class);
+
+//        Expect
+        assertThat(customerEdit.getEditedEntity()).isEqualTo(firstCustomer);
+    }
+
+    private void selectCustomerInTable(CustomerBrowse customerBrowse, Customer firstCustomer) {
+        Table<Customer> customersTable = getCustomersTable(customerBrowse);
+        assert customersTable != null;
+        customersTable.setSelected(firstCustomer);
+    }
+
+    @NotNull
+    private <T> T getScreenOfType(Screens screens, Class<T> tClass) {
+        Screen screen = screens.getOpenedScreens().getActiveScreens().stream().findFirst().orElseThrow();
+        assertThat(screen).isInstanceOf(tClass);
+        return (T) screen;
+    }
+
+    @Nullable
+    private Button getButton(CustomerBrowse customerBrowse, String buttonId) {
+        return (Button) customerBrowse.getWindow().getComponent(buttonId);
+     }
 
     @NotNull
     private Customer getFirstLoadedCustomer(CustomerBrowse customerBrowse) {
